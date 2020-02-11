0b85bc67cff07f9a3ca60a440172f60eb680452d
Vincent Zhang <iamkaruto@gmail.com>
2020-02-11 08:23:09 -0800

    Changed app.css directory

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './components/Page/Page';
-import './normalize.css'; // global normalize
-import './styles/app.css';
+import './normalize.css';
+import './app.css';
 
 ReactDOM.render(<Page />, document.getElementById('root'));
