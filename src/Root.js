d6ae8fbb0f8c07e52a8499bd285c5b37bade9aeb
Vincent Zhang <iamkaruto@gmail.com>
2020-02-11 08:20:18 -0800

    Change normalize ref

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './components/Page/Page';
-import './Root.css'; // global normalize
+import './normalize.css'; // global normalize
 import './styles/app.css';
 
 ReactDOM.render(<Page />, document.getElementById('root'));
