25af32b8a230831d1413cd0cf25a53dd6cb0621b
Vincent Zhang <iamkaruto@gmail.com>
2020-02-10 22:35:01 -0800

    Simplify app

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,37 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../Footer/Footer';
 import styles from './Page.css';
 
-class Page extends React.Component {
-    constructor(props) {
-        super(props);
+function Page() {
+    const strings = {
+        headingGlobal: 'GitHub Static Site Template',
+        subheadingGlobal: 'A React-based template for GitHub Pages. Start editing away!'
+    };
 
-        this.config = {
-            isContentVisible: true
-        };
-
-        this.strings = {
-            headingGlobal: 'GitHub Static Site Template',
-            subheadingGlobal: 'A React-based template for GitHub Pages. Start editing away!'
-        };
-    }
-
-    render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.overlay}>
-                    <header className={styles.titles}>
-                        <h1 className={styles['heading--titles']}>{this.strings.headingGlobal}</h1>
-                        <h3 className={styles['subheading--titles']}>{this.strings.subheadingGlobal}</h3>
-                    </header>
-                </div>
-                <div className={styles['container--contents']}>
-                    {this.config.isContentVisible ? <h1>Hello {this.strings.headingGlobal}</h1> : null}
-                </div>
-                <Footer />
+    return (
+        <div className={styles.container}>
+            <div className={styles.overlay}>
+                <header className={styles.titles}>
+                    <h1 className={styles['heading--titles']}>{strings.headingGlobal}</h1>
+                    <h3 className={styles['subheading--titles']}>{strings.subheadingGlobal}</h3>
+                </header>
             </div>
-        );
-    }
+            <div className={styles['container--contents']}>
+                <h1>Hello world!</h1>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default Page;
